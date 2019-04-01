4c509bf4d6aa5589bd848a0f55ec77bd403a41a2
safe <krishnakant.singh.min12@gmail.com>
2019-04-01 14:44:01 +0530

    add function to get details of user joining chatroom

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,48 +1,55 @@
 const users = [];
 
-//addUser,removeUser,getUser,getUsersInRoom
-
 const addUser = ({ id, username, room }) => {
-  //clean the data
+  // Clean the data
   username = username.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
-  //validate the data
+  // Validate the data
   if (!username || !room) {
     return {
-      error: "username and room is required"
+      error: "Username and room are required!"
     };
   }
 
-  //check for existing user
+  // Check for existing user
   const existingUser = users.find(user => {
-    return user.username === username && user.room === room;
+    return user.room === room && user.username === username;
   });
 
-  //validate username
+  // Validate username
   if (existingUser) {
     return {
-      error: "username is in use!"
+      error: "Username is in use!"
     };
   }
 
-  //store user
+  // Store user
   const user = { id, username, room };
   users.push(user);
-  return user;
+  return { user };
 };
 
-addUser({
-  id: 11,
-  username: "krishna",
-  room: "South Philly"
-});
-
 const removeUser = id => {
-  //finde index in an array
   const index = users.findIndex(user => user.id === id);
 
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
+};
+
+const getUser = id => {
+  return users.find(user => user.id === id);
+};
+
+const getUsersInRoom = room => {
+  room = room.trim().toLowerCase();
+  return users.filter(user => user.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
 };
